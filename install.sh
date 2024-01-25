01167aaae4e08427694d3d4008cc5f6d910851e8
Wellington Lopes <wellington.lopes.tw@gmail.com>
2024-01-25 00:00:46 -0300

    update

--- install.sh
+++ install.sh
@@ -29,6 +29,8 @@ asdf install nodejs latest
 asdf global golang latest
 asdf global nodejs latest
 
+go install github.com/go-delve/delve/cmd/dlv@latest
+
 git clone https://github.com/wellingtonlope/my-neovim.git
 mkdir -p ~/.config/nvim
 cp -R my-neovim/* ~/.config/nvim
