b6bb9f50808bfbb2c48781db5d5964e064d6c922
Wellington Lopes <wellington.lopes.tw@gmail.com>
2024-02-09 10:46:58 -0300

    fix: brew packages not found

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ mkdir ~/.scripts
 
 curl -L git.io/antigen >~/.scripts/antigen.zsh
 curl -L https://raw.githubusercontent.com/wellingtonlope/dotfiles/main/.ideavimrc >~/.ideavimrc
-curl -L https://raw.githubusercontent.com/wellingtonlope/dotfiles/main/.zshrc >~/.zshrc
+curl -L https://raw.githubusercontent.com/wellingtonlope/dotfiles/main/.zshrc >>~/.zshrc
 curl -L https://raw.githubusercontent.com/wellingtonlope/dotfiles/main/.tmux.conf >~/.tmux.conf
 # curl -L https://raw.githubusercontent.com/wellingtonlope/dotfiles/main/.p10k.zsh >~/.p10k.zsh
 
