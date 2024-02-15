9e4db16ed99f3428f262d4bdce1fe456c9912d8c
Wellington Lopes <wellington.lopes.tw@gmail.com>
2024-02-15 20:39:28 -0300

    feat(ollama): improving transalte and grammar

--- nvim/lua/plugins/ollama.lua
+++ nvim/lua/plugins/ollama.lua
@@ -2,11 +2,11 @@ return {
   "David-Kunz/gen.nvim",
   config = function()
     require("gen").setup({
-      model = "llama2",      -- The default model to use.
-      display_mode = "float", -- The display mode. Can be "float" or "split".
-      show_prompt = false,    -- Shows the Prompt submitted to Ollama.
-      show_model = false,     -- Displays which model you are using at the beginning of your chat session.
-      no_auto_close = false,  -- Never closes the window automatically.
+      model = "dolphin-mistral", -- The default model to use.
+      display_mode = "float",    -- The display mode. Can be "float" or "split".
+      show_prompt = false,       -- Shows the Prompt submitted to Ollama.
+      show_model = false,        -- Displays which model you are using at the beginning of your chat session.
+      no_auto_close = false,     -- Never closes the window automatically.
       init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
       -- Function to initialize Ollama
       command = "curl --silent --no-buffer -X POST http://localhost:11434/api/generate -d $body",
@@ -21,31 +21,48 @@ return {
     vim.keymap.set("n", "<leader>oc", ":Gen Chat<CR>", { buffer = buffer, desc = "Chat" })
     vim.keymap.set("n", "<leader>og", ":Gen Generate<CR>", { buffer = buffer, desc = "Generate" })
     vim.keymap.set("v", "<leader>oa", ":Gen Ask<CR>", { buffer = buffer, desc = "Ask" })
-    vim.keymap.set("v", "<leader>og", ":Gen Enhance_Grammar_Spelling<CR>", { buffer = buffer, desc = "Enhance Grammar" })
     vim.keymap.set("v", "<leader>oi", ":Gen Enhance_Code<CR>", { buffer = buffer, desc = "Improve Code" })
     vim.keymap.set("v", "<leader>or", ":Gen Review_Code<CR>", { buffer = buffer, desc = "Review Code" })
 
-    local gen                           = require("gen")
-    gen.prompts["Translate_to_English"] = {
+    local gen                               = require("gen")
+    gen.prompts["Improve_Grammar_Spelling"] = {
       prompt =
-      "Translate the following text to English, improving grammar and spelling, just output the final text without additional quotes around it:\n$text",
+      "Check the following sentence for grammar and clarity: \"$text\". Rewrite it for better readability while maintaining its original meaning. The result should be just the new sentence without additional details and explanation.",
       replace = true,
     }
-    vim.keymap.set("v", "<leader>oe", ":Gen Translate_to_English<CR>", { buffer = buffer, desc = "To English" })
+    vim.keymap.set("v", "<leader>og", ":Gen Improve_Grammar_Spelling<CR>", { buffer = buffer, desc = "Improve Grammar" })
 
-    gen.prompts["Translate_to_Spanish"] = {
+    gen.prompts["Translate_Portuguese_to_English"] = {
       prompt =
-      "Translate the following text to Spanish, improving grammar and spelling, just output the final text without additional quotes around it:\n$text",
+      "Translate the following sentence \"$text\" from Brazilian Portuguese to American English. The result should be just the translation without additional details and explanation. Use appropriate cultural references and idioms when possible.",
       replace = true,
     }
-    vim.keymap.set("v", "<leader>os", ":Gen Translate_to_Spanish<CR>", { buffer = buffer, desc = "To Spanish" })
+    vim.keymap.set("v", "<leader>otpe", ":Gen Translate_Portuguese_to_English<CR>",
+      { buffer = buffer, desc = "English" })
 
-    gen.prompts["Translate_to_Portuguese"] = {
+    gen.prompts["Translate_Portuguese_to_Spanish"] = {
       prompt =
-      "Translate the following text to Brazilian Portuguese, improving grammar and spelling, just output the final text without additional quotes around it:\n$text",
+      "Translate the following sentence \"$text\" from Brazilian Portuguese to Spanish. The result should be just the translation without additional details and explanation. Use appropriate cultural references and idioms when possible.",
       replace = true,
     }
-    vim.keymap.set("v", "<leader>op", ":Gen Translate_to_Portuguese<CR>", { buffer = buffer, desc = "To Portuguese" })
+    vim.keymap.set("v", "<leader>otps", ":Gen Translate_Portuguese_to_Spanish<CR>",
+      { buffer = buffer, desc = "Spanish" })
+
+    gen.prompts["Translate_English_to_Portuguese"] = {
+      prompt =
+      "Translate the following sentence \"$text\" from English to Brazilian Portuguese. The result should be just the translation without additional details and explanation. Use appropriate cultural references and idioms when possible.",
+      replace = true,
+    }
+    vim.keymap.set("v", "<leader>otep", ":Gen Translate_English_to_Portuguese<CR>",
+      { buffer = buffer, desc = "Portuguese" })
+
+    gen.prompts["Translate_Spanish_to_Portuguese"] = {
+      prompt =
+      "Translate the following sentence \"$text\" from Spanish to Brazilian Portuguese. The result should be just the translation without additional details and explanation. Use appropriate cultural references and idioms when possible.",
+      replace = true,
+    }
+    vim.keymap.set("v", "<leader>otsp", ":Gen Translate_Spanish_to_Portuguese<CR>",
+      { buffer = buffer, desc = "Portuguese" })
 
     gen.prompts["do_something"] = {
       prompt =
@@ -59,6 +76,26 @@ return {
       mode = { "v", "n" },
       "<leader>o",
       desc = "Ollama",
+    },
+    {
+      mode = { "v" },
+      "<leader>ot",
+      desc = "Translate",
+    },
+    {
+      mode = { "v" },
+      "<leader>ote",
+      desc = "English",
+    },
+    {
+      mode = { "v" },
+      "<leader>ots",
+      desc = "Spanish",
+    },
+    {
+      mode = { "v" },
+      "<leader>otp",
+      desc = "Portuguese",
     },
   }
 }
