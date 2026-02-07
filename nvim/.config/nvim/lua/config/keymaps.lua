dc640abb6e481511e6e8846d47af602a2965ccb4
Wellington Lopes <wellington.lopes.tw@gmail.com>
2026-02-07 11:01:39 -0300

    refactor(nvim): comment out opencode keymap

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>aa", function()
-	require("snacks").terminal({ "opencode" }, {
-		cwd = vim.fn.getcwd(),
-		border = "rounded",
-	})
-end, { desc = "OpenCode (cwd)" })
+
+-- vim.keymap.set("n", "<leader>aa", function()
+-- 	require("snacks").terminal({ "opencode" }, {
+-- 		cwd = vim.fn.getcwd(),
+-- 		border = "rounded",
+-- 	})
+-- end, { desc = "OpenCode (cwd)" })
